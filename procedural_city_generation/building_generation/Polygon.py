c82a4906d01207060e33a7f780dc75b1c401e316
kemaltyp <salarianeggs@t-online.de>
2015-07-29 12:51:29 +0200

    some tree-stuff

--- procedural_city_generation/building_generation/Polygon.py
+++ procedural_city_generation/building_generation/Polygon.py
@@ -1,5 +1,5 @@
 class Polygon(object):
-	def __init__(self, verts, faces, texture, shrinkwrap):
+	def __init__(self, verts, faces, texture, shrinkwrap=false):
 		self.verts=verts
 		self.faces=faces
 		self.shrinkwrap=shrinkwrap
