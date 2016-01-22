99e4b53313738cc140c8c7721436b38a84766d2e
Jonathan <josaudergh@gmail.com>
2016-01-22 15:34:35 -0500

    Automatic Doc Update

--- _static/sidebar.js
+++ _static/sidebar.js
@@ -42,7 +42,7 @@ $(function() {
 
   // original margin-left of the bodywrapper and width of the sidebar
   // with the sidebar expanded
-  var bw_margin_expanded = bodywrapper.css('margin-right');
+  var bw_margin_expanded = bodywrapper.css('margin-left');
   var ssb_width_expanded = sidebar.width();
 
   // margin-left of the bodywrapper and width of the sidebar
@@ -68,38 +68,38 @@ $(function() {
   function collapse_sidebar() {
     sidebarwrapper.hide();
     sidebar.css('width', ssb_width_collapsed);
-    bodywrapper.css('margin-right', bw_margin_collapsed);
+    bodywrapper.css('margin-left', bw_margin_collapsed);
     sidebarbutton.css({
-        'margin-right': '0',
+        'margin-left': '0',
         'height': bodywrapper.height()
     });
-    sidebarbutton.find('span').text('«');
+    sidebarbutton.find('span').text('»');
     sidebarbutton.attr('title', _('Expand sidebar'));
     document.cookie = 'sidebar=collapsed';
   }
 
   function expand_sidebar() {
-    bodywrapper.css('margin-right', bw_margin_expanded);
+    bodywrapper.css('margin-left', bw_margin_expanded);
     sidebar.css('width', ssb_width_expanded);
     sidebarwrapper.show();
     sidebarbutton.css({
-        'margin-right': ssb_width_expanded-12,
+        'margin-left': ssb_width_expanded-12,
         'height': bodywrapper.height()
     });
-    sidebarbutton.find('span').text('»');
+    sidebarbutton.find('span').text('«');
     sidebarbutton.attr('title', _('Collapse sidebar'));
     document.cookie = 'sidebar=expanded';
   }
 
   function add_sidebar_button() {
     sidebarwrapper.css({
-        'float': 'right',
-        'margin-left': '0',
+        'float': 'left',
+        'margin-right': '0',
         'width': ssb_width_expanded - 28
     });
     // create the button
     sidebar.append(
-        '<div id="sidebarbutton"><span>&raquo;</span></div>'
+        '<div id="sidebarbutton"><span>&laquo;</span></div>'
     );
     var sidebarbutton = $('#sidebarbutton');
     light_color = sidebarbutton.css('background-color');
@@ -118,12 +118,12 @@ $(function() {
     sidebarbutton.attr('title', _('Collapse sidebar'));
     sidebarbutton.css({
         'color': '#FFFFFF',
-        'border-right': '1px solid ' + dark_color,
+        'border-left': '1px solid ' + dark_color,
         'font-size': '1.2em',
         'cursor': 'pointer',
         'height': bodywrapper.height(),
         'padding-top': '1px',
-        'margin-right': ssb_width_expanded - 12
+        'margin-left': ssb_width_expanded - 12
     });
 
     sidebarbutton.hover(
